b8a1e41a497b18469c79b64999f3f764cd58a458
FieryYTGithub <fierydiamondfire@gmail.com>
2025-01-14 12:59:33 -0500

    FINALLY!!! DATA FROM SUPABASE!!!
    YOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- plotinum/plotsites/data.js
+++ plotinum/plotsites/data.js
@@ -1,19 +1,3 @@
-// Request the plot you want to view
-var plotid = prompt("What is the plot you want to view?", "Any registered plot ID")
-// Get the data from Supabase
-// Init a spuabase (amazing spelling) client
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://ioqyyizmfzdldbolttdp.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-// Get the thing... i keep forgetting- oh yeah the data
-const { data, error } = await supabase
-  .from('plots')
-  .select()
-  .eq('plotid', plotid)
-const recievedJson = data;
-  // Debugging
-document.getElementById("rawjson").innerHTML = JSON.stringify(receivedJson, null, 2);
 // Parse the JSON, parse it NOW.
 const obj = JSON.parse('{"id":9066, "name":"Wallrunning", "owner":"__Fiery", "node":"1", "tags":"Parkour, Roleplay, Miscellaneous", "icon":"Reinforced_Deepslate"}'); // DF needs this. I dont wanna use templates
 
