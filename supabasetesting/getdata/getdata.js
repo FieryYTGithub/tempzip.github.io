b15fac248efe51ef47dde335b1e6d39a6d81bfb8
FieryYTGithub <fierydiamondfire@gmail.com>
2025-01-13 19:13:52 -0500

    actually added node_modules

--- supabasetesting/getdata/getdata.js
+++ supabasetesting/getdata/getdata.js
@@ -1,5 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = 'https://jrrmnluhzleryemrjhrl.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+const { data, error } = await supabase
+  .from('definetly a real table')
+  .select()
+
+document.getElementById("data").innerHTML = data
+document.getElementById("error").innerHTML = error
